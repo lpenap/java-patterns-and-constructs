2a2686b493d14b46f3a2ba24f235090f8f45bdd7
Luis Augusto Peña Pereira <lpenap@gmail.com>
2019-08-09 17:22:55 -0300

    Adding app test

--- src/test/java/com/penapereira/example/constructs/JavaPatternsAndConstructsApplicationTests.java
+++ src/test/java/com/penapereira/example/constructs/JavaPatternsAndConstructsApplicationTests.java
@@ -3,10 +3,12 @@ package com.penapereira.example.constructs;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@ContextConfiguration(loader = CustomSpringBootContextLoader.class)
 public class JavaPatternsAndConstructsApplicationTests {
 
 	@Test
